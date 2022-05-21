163cdf72ce4d85e1f3d9fb8ad4adcfb89e616039
nikhil yadav <iamnikhilyadav27@gmail.com>
2022-05-21 16:17:14 +0530

    getWork assingment done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from "./data";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //state to store the data
+  const [dataFetched, setDataFetched] = useState([...data]);
+
+  const handleChange = (e) => {
+    const { name, checked } = e.target; //extracting the name and checked property from selected item
+    //console.log(name);
+
+    //if its a course ie select all button then looping over all the branches of the course and mutating the isChecked to either true or false depending on the checked value, later updating the whole objects isChecked value
+    if (name === "Btech" || name === "Law" || name === "Medical") {
+      let tempData = dataFetched.map((data) => {
+        let branches =
+          name === data.course
+            ? data.branches.map((branch) => {
+                return { ...branch, isChecked: checked };
+              })
+            : data.branches;
+        return {
+          ...data,
+          branches,
+          isChecked: !branches.some(
+            (branch) => branch?.isChecked !== true // checking if any of the branchs' isChecked value is not true, if all true then only setting it to true otherwise false
+          ),
+        };
+      });
+      setDataFetched(tempData); //saving the processed data to our state
+      //console.log(tempData);
+      //console.log(dataFetched);
+    }
+    // if the selected item is just a branch then mutating only the branchs isChecked value
+    else {
+      let tempData = dataFetched.map((data) => {
+        let branches = data.branches.map((branch) =>
+          branch.branchName === name
+            ? { ...branch, isChecked: checked }
+            : { ...branch }
+        );
+        return {
+          ...data,
+          branches,
+          isChecked: !branches.some((branch) => branch?.isChecked !== true),
+        };
+      });
+      setDataFetched(tempData);
+      //console.log(tempData);
+      //console.log(dataFetched);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/******************* randering the checkbox form ************************/}
+      <div className="mx-4 my-4">
+        <form className="form w-100">
+          <h3>Select Courses and Branches</h3>
+
+          {dataFetched.map(({ course, branches, id }) => {
+            return (
+              <>
+                <div className="form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    name={course}
+                    checked={
+                      !branches.some((branch) => branch?.isChecked !== true)
+                    }
+                    onChange={handleChange}
+                    key={id}
+                  />
+                  <label className="form-check-label ms-2">{course}</label>
+                </div>
+
+                {branches.map((branch, id) => {
+                  return (
+                    <div className="form-check" style={{ marginLeft: "20px" }}>
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                        name={branch.branchName}
+                        checked={branch?.isChecked || false}
+                        onChange={handleChange}
+                        key={id}
+                      />
+                      <label className="form-check-label ms-2">
+                        {branch.branchName}
+                      </label>
+                    </div>
+                  );
+                })}
+              </>
+            );
+          })}
+        </form>
+      </div>
+
+      {/******************* diplaying the  selected items *****************/}
+      <div style={{ padding: "20px" }}>
+        {dataFetched.map((data) => {
+          return data.isChecked === true ? (
+            <button className="chip-btn">
+              {data.course + " All branches"}
+            </button>
+          ) : (
+            data.branches.map((branch) => {
+              return (
+                branch.isChecked === true && (
+                  <button className="chip-btn">{branch.branchName}</button>
+                )
+              );
+            })
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    id: "Btech101",
+    course: "Btech",
+    isChecked: false,
+    branches: [
+      { id: "me-1", branchName: "ME", isChecked: false },
+      { id: "ce-1", branchName: "CE", isChecked: false },
+      { id: "ece-1", branchName: "ECE", isChecked: false },
+      { id: "ee-1", branchName: "EE", isChecked: false },
+      { id: "cse-1", branchName: "CSE", isChecked: false },
+      { id: "it-1", branchName: "IT", isChecked: false },
+    ],
+  },
+  {
+    id: "Law101",
+    course: "Law",
+    isChecked: false,
+    branches: [
+      { id: "bl-1", branchName: "Business Law", isChecked: false },
+      { id: "cl-1", branchName: "Civil Law", isChecked: false },
+      { id: "cyl-1", branchName: "Cyber Law", isChecked: false },
+      { id: "col-1", branchName: "Corporate Law", isChecked: false },
+      { id: "cril-1", branchName: "Criminal Law", isChecked: false },
+      { id: "con-1", branchName: "Constitutional Law", isChecked: false },
+      { id: "tl-1", branchName: "Tax Law", isChecked: false },
+    ],
+  },
+  {
+    id: "medical101",
+    course: "Medical",
+    isChecked: false,
+    branches: [
+      { id: "mbbs-1", branchName: "MBBS", isChecked: false },
+      { id: "ms-1", branchName: "MS", isChecked: false },
+      { id: "md-1", branchName: "MD", isChecked: false },
+      { id: "bams-1", branchName: "BAMS", isChecked: false },
+      { id: "bhms-1", branchName: "BHMS", isChecked: false },
+      { id: "bpt-1", branchName: "BPT", isChecked: false },
+    ],
+  },
+];
